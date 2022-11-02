489f5108b285cf04ccabae5e14c080b93bcfa203
unknown <houssembenmammarinfo@gmail.com>
2022-11-02 23:16:27 +0100

    services finished

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,11 @@ import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
 import heroImg from '../assets/images/hero-img.png'
 import '../styles.css/home.css'
+import Services from '../services/Services'
 
 
 function Home() {
+    const year = new Date().getFullYear()
   return (
     <Helmet title="home">
      <section className="hero__section">
@@ -15,7 +17,7 @@ function Home() {
                 <Row>
                     <Col sm="12" lg="6" md="6" >
                         <div className="hero__content">
-                            <p>Trending production in 2022</p>
+                            <p>Trending production in {year}</p>
                             <h2>Make Your Interior More Minimalistic & Modern</h2>
                             <p>Lorem Ipsum has been dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it
                                 to make a type specimen book. </p>
@@ -29,6 +31,9 @@ function Home() {
                 </Row>
             </Container >
         </section >
+        <Services>
+            
+        </Services>
     </Helmet>
   )
 }
